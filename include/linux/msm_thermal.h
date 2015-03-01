eabb1d64ac8ea501388da4c99d543273a723ce2f
zparallax <zparallax1@gmail.com>
2015-02-28 21:32:23 -0600

    Added FauxSound Added Intellithermal

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -38,9 +38,12 @@ struct msm_thermal_data {
 	int32_t ocr_temp_hyst_degC;
 };
 
-#ifdef CONFIG_THERMAL_MONITOR
+
+#if defined(CONFIG_THERMAL_MONITOR) || defined(CONFIG_INTELLI_THERMAL_V2)
 extern int msm_thermal_init(struct msm_thermal_data *pdata);
+#ifdef CONFIG_THERMAL_MONITOR
 extern int msm_thermal_device_init(void);
+#endif
 extern int msm_thermal_set_frequency(uint32_t cpu, uint32_t freq,
 	bool is_max);
 #else
